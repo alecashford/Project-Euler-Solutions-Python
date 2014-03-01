d869a7b87608db519ce9b5a7ea01577f3ac39971
zagadooga <ashford91@gmail.com>
2014-02-28 19:53:13 -0800

    Update euler2.py

--- euler2.py
+++ euler2.py
@@ -3,7 +3,7 @@ last = 0
 before_last = 0
 
 for counter in range(0,10):
-	before_laste = last
-	last = number
-	number = before_last + last
-	print (number)
+    before_last = last
+    last = number
+    number = before_last + last
+    print (number)
